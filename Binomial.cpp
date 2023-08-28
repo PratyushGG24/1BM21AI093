9ff675850e6d01d75be24935f33f333993c08d48
PratyushGG24 <142788718+PratyushGG24@users.noreply.github.com>
2023-08-28 15:34:49 +0530

    Binomial.cpp

--- Binomial.cpp
+++ Binomial.cpp
@@ -1,25 +1,41 @@
-#include <bits/stdc++.h>
 
+#include <bits/stdc++.h>
 using namespace std;
 
-    int binomialCoeff(int n, int k)
+
+int min(int a, int b);
+
+
+int binomialCoeff(int n, int k)
 {
-    
-    if (k > n)
-        return 0;
-    if (k == 0 || k == n)
-        return 1;
- 
-    
-    return binomialCoeff(n - 1, k - 1)
-           + binomialCoeff(n - 1, k);
+	int C[n + 1][k + 1];
+	int i, j;
+
+
+	for (i = 0; i <= n; i++) {
+		for (j = 0; j <= min(i, k); j++) {
+			// Base Cases
+			if (j == 0 || j == i)
+				C[i][j] = 1;
+
+			
+			else
+				C[i][j] = C[i - 1][j - 1] + C[i - 1][j];
+		}
+	}
+
+	return C[n][k];
 }
- 
+
+
+int min(int a, int b) { return (a < b) ? a : b; }
+
 
 int main()
 {
-    int n = 5, k = 2;
-    cout << "Value of C(" << n << ", " << k << ") is "
-         << binomialCoeff(n, k);
-    return 0;  
+	int n = 5, k = 2;
+	cout << "Value of C[" << n << "][" << k << "] is "
+		<< binomialCoeff(n, k);
 }
+
+
